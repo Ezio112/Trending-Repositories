e60d082de9e9029d8f1cf1060b4c83b5175dd1c7
Ezio112 <surajnehra112@gmail.com>
2020-11-21 20:58:06 +0530

    Constructor for App class and Api calling were coded also variable for loading while fetching data initialised

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      DataFromApi:{},
+      IsLoading:true
+    };
+  }
+  componentDidMount(){
+    fetch("https://api.wheretheiss.at/v1/satellites/25544").then(response=>response.json())
+    .then(data=>{
+      this.setState(prevState=>{
+        return {DataFromApi:data,IsLoading:false};
+      });//Closing of setState function
+    });//closing of promise
+  }
   render(){
+    if(this.state.IsLoading){
+      return (<h1>Loading...</h1>);
+    }
+    //If data is fetching is successfull below code will be rendered
     return (<h1>Hi</h1>);
   }
 }

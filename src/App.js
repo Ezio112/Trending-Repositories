e466187e684bc992ae47f5b93fb667a316fe402e
Ezio112 <surajnehra112@gmail.com>
2020-11-21 22:50:44 +0530

    Did all the styling imported bootstrap and reactstrap used button and Collapse to make tile function.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import React from "react";
 import RepositoryTile from "./RepositoryTile.js";
 import OfflineData from "./OfflineData";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 class App extends React.Component{
   constructor(){
     super();
     this.state={
       DataFromApi:OfflineData,
-      IsLoading:true
+      IsLoading:true,
+      DataLoadingSuccessfull:true
     };
+    //RepositoryTiles is an array of tiles that will be displayed on the screen.
     this.RepositoryTiles=this.state.DataFromApi.map(item=><RepositoryTile details={item}/>);
   }
+
   componentDidMount(){
-    fetch("https://api.wheretheiss.at/v1/satellites/25544").then(response=>response.json())
+    //Sending request to API
+    fetch("https://api.wheretheiss.at/v1/satellites/25544").then(response=>{
+      if(!response.ok){
+        this.setState(prevState=>{return{DataFromApi:prevState.DataFromApi,IsLoading:prevState.IsLoading,DataLoadingSuccessfull:false}});
+      }
+      else
+        return response.json()
+    })//Closing of promise we checked if response was okay are if had some error
     .then(data=>{
       this.setState(prevState=>{
-        return {DataFromApi:data,IsLoading:false};
+        //As soon as the data is loaded it is stored in state.DataFromApi
+        return {DataFromApi:data,IsLoading:false,DataLoadingSuccessfull:true};
       });//Closing of setState function
-    });//closing of promise
+    })//closing of promise
+    .catch(error =>{
+      this.setState(prevState=>{return{DataFromApi:prevState.DataFromApi,IsLoading:prevState.IsLoading,DataLoadingSuccessfull:false}});
+    });//Closing of catch statement
   }
+
+
   render(){
+    //This code will be rendered while the fetching is in going on.
     if(this.state.IsLoading){
       return (<h1>Loading...</h1>);
     }
     //If data is fetching is successfull below code will be rendered
     return (
-      <div>
+      <div className="container">
         {this.RepositoryTiles}
       </div>
     );
--- src/RepositoryTile.js
+++ src/RepositoryTile.js
@@ -1,10 +1,45 @@
 import React from "react";
+import {Collapse,Button, CardBody, Card} from 'reactstrap';
+import "./index.css";
 
 class RepositoryTile extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      IsOpen:false
+    };
+    this.toggle=this.toggle.bind(this);
+  }
+  //This function toggles the state of the collapsed element.
+  toggle(){
+    console.log("A tile was Clicked!");
+    this.setState(prevState=> {return {IsOpen:!prevState.IsOpen}});
+  }
+
+
   render(){
     return(
-      <p>Data</p>
-    )
+      <div className="container-lg">
+        <button type="button" className="collapsible" onClick={this.toggle}>
+          <img src={this.props.details.avatar} className="icon"/>
+          <p style={{color:"grey"}}>{this.props.details.author}</p>
+          <h3>{this.props.details.name}</h3>
+        </button>
+
+        <Collapse isOpen={this.state.IsOpen}>
+          <div className="content">
+            <p style={{color:"grey",paddingLeft:"100px"}}>{this.props.details.description}</p>
+            <br></br>
+            <ul style={{paddingLeft:"100px","list-style-type":"none"}}>
+              <li style={{color:this.props.details.languageColor,float:"left",width:"33%"}}>{this.props.details.language}</li>
+              <li style={{float:"left",width:"33%"}}>
+              <img style={{width:"20px",height:"20px"}}src="https://image.flaticon.com/icons/svg/148/148841.svg"/>{this.props.details.stars}</li>
+              <li style={{float:"right",width:"33%"}}><img  style={{width:"20px",height:"20px"}} src="https://maxcdn.icons8.com/Android_L/PNG/48/Programming/git_fork-48.png"/>{this.props.details.forks}</li>
+            </ul>
+          </div>
+        </Collapse>
+      </div>
+    );
   }
 }
 export default RepositoryTile;
